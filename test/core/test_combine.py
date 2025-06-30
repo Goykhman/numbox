bf359220adf55de7317edf15bfd1b0c51d45c1b7
Mikhail Goykhman <mikhailgoykhman@MacBookPro.lan>
2025-06-29 21:22:37 -0400

    improved combine search by stopping after locating all requested nodes

--- test/core/test_combine.py
+++ test/core/test_combine.py
@@ -75,5 +75,19 @@ def test_combine_3():
     assert isclose(collected[0]["w3"], 1.72 + 2 * 1.41)
 
 
+def test_combine_4():
+    w1 = make_work_helper("w1", 1.41)
+    w1_pretend = make_work_helper("w1", 1.72)
+    w2 = make_work_helper("w2", 0.0, sources=(w1,), derive_py=lambda w1_: w1_)
+    w3 = make_work_helper("w3", 0.0, sources=(w1_pretend,), derive_py=lambda w1_: w1_)
+    w4 = make_work_helper("w4", 0.0, sources=(w2, w3), derive_py=lambda w2_, w3_: (w2_ + w3_) / 2.0)
+    sheaf = make_sheaf_dict(w1)
+    w4.combine(sheaf)
+    w1_collected = sheaf["w1"].get_as(typeof(w1.data))
+    if not isclose(w1_collected, 1.41):
+        assert isclose(w1_collected, 1.72), f"something else went wrong... got {w1_collected}"
+        raise RuntimeError("didn't stop after locating 'w1' in `w1` via `w2`, went on to `w1_pretend` via `w3`")
+
+
 if __name__ == "__main__":
     collect_and_run_tests(__name__)
