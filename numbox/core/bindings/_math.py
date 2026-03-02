1f7d9bc13f599f6ea98517aaaa631f99b744e12c
Nelson, Erik <erik_lee_nelson@yahoo.com>
2026-03-01 20:17:46 -0500

    added a few libm bindings

--- numbox/core/bindings/_math.py
+++ numbox/core/bindings/_math.py
@@ -20,3 +20,153 @@ def sin(x):
 @cres(signatures.get("tan"), cache=True)
 def tan(x):
     return _call_lib_func("tan", (x,))
+
+
+@cres(signatures.get("acos"), cache=True)
+def acos(x):
+    return _call_lib_func("acos", (x,))
+
+
+@cres(signatures.get("asin"), cache=True)
+def asin(x):
+    return _call_lib_func("asin", (x,))
+
+
+@cres(signatures.get("atan"), cache=True)
+def atan(x):
+    return _call_lib_func("atan", (x,))
+
+
+@cres(signatures.get("cosh"), cache=True)
+def cosh(x):
+    return _call_lib_func("cosh", (x,))
+
+
+@cres(signatures.get("sinh"), cache=True)
+def sinh(x):
+    return _call_lib_func("sinh", (x,))
+
+
+@cres(signatures.get("tanh"), cache=True)
+def tanh(x):
+    return _call_lib_func("tanh", (x,))
+
+
+@cres(signatures.get("acosh"), cache=True)
+def acosh(x):
+    return _call_lib_func("acosh", (x,))
+
+
+@cres(signatures.get("asinh"), cache=True)
+def asinh(x):
+    return _call_lib_func("asinh", (x,))
+
+
+@cres(signatures.get("atanh"), cache=True)
+def atanh(x):
+    return _call_lib_func("atanh", (x,))
+
+
+@cres(signatures.get("exp"), cache=True)
+def exp(x):
+    return _call_lib_func("exp", (x,))
+
+
+@cres(signatures.get("exp2"), cache=True)
+def exp2(x):
+    return _call_lib_func("exp2", (x,))
+
+
+@cres(signatures.get("expm1"), cache=True)
+def expm1(x):
+    return _call_lib_func("expm1", (x,))
+
+
+@cres(signatures.get("log"), cache=True)
+def log(x):
+    return _call_lib_func("log", (x,))
+
+
+@cres(signatures.get("log2"), cache=True)
+def log2(x):
+    return _call_lib_func("log2", (x,))
+
+
+@cres(signatures.get("log10"), cache=True)
+def log10(x):
+    return _call_lib_func("log10", (x,))
+
+
+@cres(signatures.get("log1p"), cache=True)
+def log1p(x):
+    return _call_lib_func("log1p", (x,))
+
+
+@cres(signatures.get("logb"), cache=True)
+def logb(x):
+    return _call_lib_func("logb", (x,))
+
+
+@cres(signatures.get("sqrt"), cache=True)
+def sqrt(x):
+    return _call_lib_func("sqrt", (x,))
+
+
+@cres(signatures.get("cbrt"), cache=True)
+def cbrt(x):
+    return _call_lib_func("cbrt", (x,))
+
+
+@cres(signatures.get("ceil"), cache=True)
+def ceil(x):
+    return _call_lib_func("ceil", (x,))
+
+
+@cres(signatures.get("floor"), cache=True)
+def floor(x):
+    return _call_lib_func("floor", (x,))
+
+
+@cres(signatures.get("trunc"), cache=True)
+def trunc(x):
+    return _call_lib_func("trunc", (x,))
+
+
+@cres(signatures.get("round"), cache=True)
+def round(x):
+    return _call_lib_func("round", (x,))
+
+
+@cres(signatures.get("rint"), cache=True)
+def rint(x):
+    return _call_lib_func("rint", (x,))
+
+
+@cres(signatures.get("nearbyint"), cache=True)
+def nearbyint(x):
+    return _call_lib_func("nearbyint", (x,))
+
+
+@cres(signatures.get("erf"), cache=True)
+def erf(x):
+    return _call_lib_func("erf", (x,))
+
+
+@cres(signatures.get("erfc"), cache=True)
+def erfc(x):
+    return _call_lib_func("erfc", (x,))
+
+
+@cres(signatures.get("lgamma"), cache=True)
+def lgamma(x):
+    return _call_lib_func("lgamma", (x,))
+
+
+@cres(signatures.get("tgamma"), cache=True)
+def tgamma(x):
+    return _call_lib_func("tgamma", (x,))
+
+
+@cres(signatures.get("fabs"), cache=True)
+def fabs(x):
+    return _call_lib_func("fabs", (x,))
--- numbox/core/bindings/signatures.py
+++ numbox/core/bindings/signatures.py
@@ -10,9 +10,47 @@ signatures_c = {
 }
 
 signatures_m = {
+    # Trig
     "cos": float64(float64),
     "sin": float64(float64),
     "tan": float64(float64),
+    # Inverse trig
+    "acos": float64(float64),
+    "asin": float64(float64),
+    "atan": float64(float64),
+    # Hyperbolic
+    "cosh": float64(float64),
+    "sinh": float64(float64),
+    "tanh": float64(float64),
+    "acosh": float64(float64),
+    "asinh": float64(float64),
+    "atanh": float64(float64),
+    # Exponential/log
+    "exp": float64(float64),
+    "exp2": float64(float64),
+    "expm1": float64(float64),
+    "log": float64(float64),
+    "log2": float64(float64),
+    "log10": float64(float64),
+    "log1p": float64(float64),
+    "logb": float64(float64),
+    # Power/root
+    "sqrt": float64(float64),
+    "cbrt": float64(float64),
+    # Rounding
+    "ceil": float64(float64),
+    "floor": float64(float64),
+    "trunc": float64(float64),
+    "round": float64(float64),
+    "rint": float64(float64),
+    "nearbyint": float64(float64),
+    # Error/gamma
+    "erf": float64(float64),
+    "erfc": float64(float64),
+    "lgamma": float64(float64),
+    "tgamma": float64(float64),
+    # Absolute value
+    "fabs": float64(float64),
 }
 
 signatures_sqlite = {
